d53ffd6fd9214f389c11934e8a65d6079fc827b4
Antonia <antonia.ivanova09@yahoo.com>
2018-09-23 15:57:05 +0200

    Extended execution test

--- Metric-Correlation-Analysis/test/metric/correlation/analysis/test/ExecutionTest.java
+++ Metric-Correlation-Analysis/test/metric/correlation/analysis/test/ExecutionTest.java
@@ -1,6 +1,8 @@
 package metric.correlation.analysis.test;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.Hashtable;
@@ -13,40 +15,75 @@ import org.junit.Test;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.github.fge.jackson.JsonLoader;
-import com.github.fge.*;
 import com.github.fge.jsonschema.exceptions.ProcessingException;
 import com.github.fge.jsonschema.main.JsonSchemaFactory;
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.stream.JsonReader;
 
 import metric.correlation.analysis.MetricCalculation;
 import metric.correlation.analysis.configuration.ProjectConfiguration;
 
 public class ExecutionTest {
 
-	 private static final Logger LOGGER = LogManager.getLogger();
+	private static final Logger LOGGER = LogManager.getLogger();
 
 	@Test
 	public void execute() throws UnsupportedOperationSystemException {
-		for (File jsonFile : new File("input").listFiles((path, name) -> name.toLowerCase().endsWith(".json"))) {
-			try {
-				JsonNode configurationNode = JsonLoader.fromFile(jsonFile);	
-				JsonNode schemaNode = JsonLoader.fromFile(new File("schema.json"));
+		try {
+			File projectsReleaseDataJSON = new File(
+					metric.projectSelection.ProjectsOutputCreator.projectsDataOutputFilePath);
 
-				if(!JsonSchemaFactory.byDefault().getValidator().validate(schemaNode, configurationNode).isSuccess()) {
-					LOGGER.log(Level.WARN, "The project configuration is not valid: "+jsonFile.getAbsolutePath());
-				}
-				else {
-					System.out.println(configurationNode.getNodeType());						
-					
-				}
+			JsonNode configurationNode = JsonLoader.fromFile(projectsReleaseDataJSON);
+			JsonNode schemaNode = JsonLoader.fromFile(new File("schema.json"));
+
+			if (!JsonSchemaFactory.byDefault().getValidator().validate(schemaNode, configurationNode).isSuccess()) {
+				LOGGER.log(Level.WARN,
+						"The project configuration is not valid: " + projectsReleaseDataJSON.getAbsolutePath());
+			} else {
+				System.out.println(configurationNode.getNodeType());
 			}
-			catch(IOException | ProcessingException e) {
-				LOGGER.log(Level.WARN, "Couldn't load project configuration from file: "+jsonFile.getAbsolutePath());
-			}
+
+		} catch (IOException | ProcessingException e) {
+			e.printStackTrace();
+			return;
 		}
-		Hashtable<String, String> mapping = new Hashtable<String, String>();
-		mapping.put("225648e16028af05ab2be93553abef1126e6dbfe", "*");
-		ProjectConfiguration config = new ProjectConfiguration("APIJSON", "TommyLemon",
-				"https://github.com/TommyLemon/APIJSON.git", mapping);
-		new MetricCalculation().calculateAll(Arrays.asList(config));
+
+		String projectsReleaseData = metric.projectSelection.ProjectsOutputCreator.projectsDataOutputFilePath;
+		Gson gson = new Gson();
+		JsonReader reader;
+
+		try {
+			reader = new JsonReader(new FileReader(projectsReleaseData));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			return;
+		}
+
+		JsonObject projectsObject = gson.fromJson(reader, JsonObject.class);
+		JsonArray projects = projectsObject.get("projects").getAsJsonArray();
+
+		for (JsonElement project : projects) {
+
+			String productName = project.getAsJsonObject().get("productName").getAsString();
+			String vendorName = project.getAsJsonObject().get("vendorName").getAsString();
+			String gitURL = project.getAsJsonObject().get("url").getAsString();
+			JsonArray commits = project.getAsJsonObject().get("commits").getAsJsonArray();
+
+			Hashtable<String, String> commitsAndVersions = new Hashtable<String, String>();
+
+			for (JsonElement commit : commits) {
+				String commitId = commit.getAsJsonObject().get("commitId").getAsString();
+				String commitVersion = commit.getAsJsonObject().get("version").getAsString();
+
+				commitsAndVersions.put(commitVersion, commitId);
+			}
+
+			ProjectConfiguration config = new ProjectConfiguration(productName, vendorName, gitURL, commitsAndVersions);
+			new MetricCalculation().calculateAll(Arrays.asList(config));
+
+		}
 	}
 }
