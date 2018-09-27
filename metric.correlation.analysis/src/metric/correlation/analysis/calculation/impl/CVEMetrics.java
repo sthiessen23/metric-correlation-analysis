e4d3a75580baf241671be6fbb3bb59a28dbb6dde
Antonia <antonia.ivanova09@yahoo.com>
2018-09-27 22:19:23 +0200

    CVE metric calculation

--- metric.correlation.analysis/src/metric/correlation/analysis/calculation/impl/CVEMetrics.java
+++ metric.correlation.analysis/src/metric/correlation/analysis/calculation/impl/CVEMetrics.java
@@ -7,27 +7,18 @@ import metric.correlation.analysis.vulnerabilities.VulnerabilityDataQueryHandler
 
 public class CVEMetrics implements IMetricCalculator {
 
+	private HashMap<String, Double> results;
+	
 	@Override
 	public boolean calculateMetric(IJavaProject project, String productName, String vendorName, String version) {
-		// TODO: Sven, how do I catch the exception that elasticsearch is not running?
 		VulnerabilityDataQueryHandler VDQH = new VulnerabilityDataQueryHandler();
-		HashMap<String, Double> results = VDQH
-				.getMetrics(VDQH.getVulnerabilities(productName, vendorName, version, "TWO"));
+		results = VDQH.getMetrics(VDQH.getVulnerabilities(productName, vendorName, version, "TWO"));
 		return !results.isEmpty();
 	}
 
-	private HashMap<String, Double> getResults(String productName, String vendorName, String version) {
-		VulnerabilityDataQueryHandler VDQH = new VulnerabilityDataQueryHandler();
-		HashMap<String, Double> results = VDQH
-				.getMetrics(VDQH.getVulnerabilities(productName, vendorName, version, "TWO"));
-		return results;
-	}
-	
-	//TODO: Sven, how do I implement this without the input?
 	@Override
 	public HashMap<String, Double> getResults() {
-		// TODO Auto-generated method stub
-		return null;
+		return results;
 	}
 
 }
